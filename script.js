ff41fc9dd64020f9d1362f819f77ff9d060519b4
Carolina Sosa <carososa123@hotmail.com>
2020-08-05 21:05:18 -0300

    version final

---
+++ script.js
@@ -0,0 +1,361 @@
+//URL ENDPOINTS
+const API_KEY = "E83LFC5hFXEDnC6KbSjVge3k89s0q5dl";
+const URL_SEARCH = "https://api.giphy.com/v1/gifs/search?";
+const URL_TRENDING = "https://api.giphy.com/v1/gifs/trending?";
+const URL_SUGERENCIAS = "https://api.giphy.com/v1/gifs/random?";
+const URL_AUTOCOMPLETE = "https://api.giphy.com/v1/gifs/search/tags?";
+const URL_SEARCH_BY_ID = "https://api.giphy.com/v1/gifs/";
+//OBJETOS DEL DOM
+let menuEstilos = document.getElementsByClassName("menu-estilos")[0];
+let cartel = document.getElementsByClassName("cartel-oops")[0];
+let botonMisGuifos = document.getElementById("mis-guifos");
+let gridMisGuifos = document.getElementById("grid-mis-guifos");
+let logo = document.getElementsByClassName("logo-claro")[0];
+let botonModoClaro = document.getElementById("sailor-day");
+let botonModoOscuro = document.getElementById("sailor-night");
+let lupa = document.getElementById("lupa");
+const POSIBLES_RESULTADOS = document.getElementsByClassName("posibles-result")[0];
+let botonPosiblesResultados1 = POSIBLES_RESULTADOS.getElementsByTagName("button")[0];
+let botonPosiblesResultados2 = POSIBLES_RESULTADOS.getElementsByTagName("button")[1];
+let botonPosiblesResultados3 = POSIBLES_RESULTADOS.getElementsByTagName("button")[2];
+const BOTON_SEARCH = document.getElementsByClassName("boton-buscar")[0];
+const SEARCH = document.getElementById("search");
+const BOTON_ESTILOS = document.getElementsByClassName("boton-elegir-tema")[0];
+var stringUser = "";
+var arrayBusquedaPrevia = [];
+const PALABRAGIF = "GIF";
+//FUNCIONES
+const FUNCION_MENU_SHOW = ()=>{
+        if(menuEstilos.className == "menu-estilos"){
+            menuEstilos.classList.replace("menu-estilos", "menu-estilos-show")
+            }else{
+            menuEstilos.classList.replace("menu-estilos-show","menu-estilos")
+        }
+};
+function botonSearchDisabled(){
+    if(BOTON_SEARCH.disabled == true){
+        if(localStorage.getItem("modoOscuro") == "false"){
+            lupa.setAttribute("src", "/images/lupa_inactive.svg");
+            }else{
+                lupa.setAttribute("src", "/images/Combined-Shape.svg")
+        }
+    }
+}
+botonSearchDisabled();
+function mostrarMisGuifos(){
+    if(localStorage.getItem("misGifs") == null){
+        cartel.style.setProperty("display", "block");
+    }else{
+        let misGifs = [];
+        misGifs = JSON.parse(localStorage.getItem("misGifs"));
+        misGifs.forEach(hacerFetchMisGuifos);
+    }
+}
+function hacerFetchMisGuifos (element){
+    fetch(URL_SEARCH_BY_ID + element +"?api_key=" + API_KEY )
+    .then((response)=>{
+        if(response.ok){
+            return response.json();
+        }
+    })
+        .then((bodyEnJson)=>{
+            displayMisGuifos(bodyEnJson.data);
+            console.log(bodyEnJson)
+        })
+}
+function displayMisGuifos(data){
+    let gifResultado = document.createElement("img");
+    let urlGif = document.createElement("a");
+    urlGif.setAttribute("href", data.embed_url)
+    urlGif.setAttribute("target", "_blank");
+    gifResultado.setAttribute("src", data.images["fixed_height"].url)
+    urlGif.appendChild(gifResultado)
+    gridMisGuifos.appendChild(urlGif);
+}
+function autocompletar (){
+    SEARCH.addEventListener("input",()=>{
+        stringUser = SEARCH.value;
+    })
+    if(stringUser !== ""){
+        BOTON_SEARCH.disabled = false;
+        if(localStorage.getItem("modoOscuro") == "false"){
+            lupa.setAttribute("src", "/images/lupa.svg");
+            }else{
+            lupa.setAttribute("src", "/images/lupa_light.svg");
+            }
+        fetch(URL_AUTOCOMPLETE + "api_key=" + API_KEY + "&q=" + stringUser )
+        .then((response)=>{
+            if(response.ok){
+                return response.json();
+            }
+        })
+            .then((responseEnJson)=>{
+                POSIBLES_RESULTADOS.getElementsByTagName("button")[0].innerText = responseEnJson.data[1].name;
+                POSIBLES_RESULTADOS.getElementsByTagName("button")[1].innerText = responseEnJson.data[2].name;
+                POSIBLES_RESULTADOS.getElementsByTagName("button")[2].innerText = responseEnJson.data[3].name;
+        })
+    }else{
+        BOTON_SEARCH.disabled = true; 
+        if(localStorage.getItem("modoOscuro") == "false"){
+            lupa.setAttribute("src", "/images/lupa_inactive.svg");
+            }else{
+                lupa.setAttribute("src", "/images/Combined-Shape.svg")
+        }
+    }};
+const mostrarSugerencias = ()=>{
+    if(SEARCH.value !== ""){
+        POSIBLES_RESULTADOS.style.setProperty("display", "block")
+    }else{
+        POSIBLES_RESULTADOS.style.setProperty("display", "none")}
+};
+function buscarGif (){
+    fetch(URL_SEARCH + "api_key=" + API_KEY + "&q=" + stringUser + "&limit=18")
+        .then((response)=>{
+            if(response.ok){
+                return response.json();
+            }
+        })
+        .then((bodyEnJson)=>{
+            if(arrayBusquedaPrevia.length == 1){
+            mostrarResultados(bodyEnJson.data);
+            }else{
+                mostrarResultadosNuevos(bodyEnJson.data);
+            }
+        })
+};
+function mostrarResultados (data){
+    let gridResultados = document.getElementsByClassName("grid-resultados")[0]
+    gridResultados.style.setProperty("display", "block")
+    for(i = 0; i < data.length; i++){
+        let gifResultado = document.createElement("img");
+        let divResultado = document.createElement("div");
+        let divHashtags = document.createElement("div");
+        let tituloGif = document.createElement("p");
+        let tituloGifContenido =  data[i].title;
+        let tituloGifFinal = tituloGifContenido.substring(0 , tituloGifContenido.indexOf(PALABRAGIF));
+        let urlGif = document.createElement("a");
+        urlGif.setAttribute("href", data[i].embed_url);
+        urlGif.setAttribute("target","_blank");
+        divResultado.setAttribute("class", "div-resultado");
+        divHashtags.setAttribute("class", "hashtags-resultado");
+        tituloGif.innerText = "#" + tituloGifFinal;
+        gifResultado.setAttribute("src", data[i].images['fixed_height'].url);
+        gifResultado.style.setProperty("height", "238px");
+        
+        divHashtags.appendChild(tituloGif)
+        divResultado.appendChild(gifResultado);
+        urlGif.appendChild(divResultado)
+        divResultado.appendChild(divHashtags);
+        document.getElementsByClassName("grid-resultados")[0].appendChild(urlGif);
+    }
+}
+function mostrarResultadosNuevos (data){
+    for (i = 0; i < data.length; i++){
+        let gifResultadoPrevio = document.getElementsByClassName("gif-resultado")[i];
+        gifResultadoPrevio.setAttribute("src", data[i].images["fixed_height"].url)
+    }
+};
+function pushBusqueda (){
+    arrayBusquedaPrevia.push(stringUser);
+    let busquedaPrevia = document.createElement("button");
+    busquedaPrevia.setAttribute("class", "busqueda-previa");
+    busquedaPrevia.innerHTML = "#" + stringUser;
+    busquedaPrevia.addEventListener("click", ()=>{
+       let stringInterno = busquedaPrevia.innerText;
+       stringFinal = stringInterno.substring(stringInterno.indexOf("#")+1, stringInterno.length);
+       stringUser = stringFinal;
+    })
+    busquedaPrevia.addEventListener("click", buscarGif)
+    busquedaPrevia.addEventListener("click", cambiarTitulo)
+    let contenedorBusquedaPrevia = document.getElementsByClassName("contenedor-busqueda-previa")[0];
+    contenedorBusquedaPrevia.appendChild(busquedaPrevia);
+    document.body.appendChild(contenedorBusquedaPrevia)
+};
+function ocultarTendencias(){
+    document.getElementById("grid-tendencias").style.setProperty("display", "none")
+};
+function ocultarAutocomplete (){
+    POSIBLES_RESULTADOS.style.setProperty("display", "none");
+};
+function cambiarTitulo(){
+    let tituloSugerencias = document.getElementsByClassName("sugerencias")[0]
+    let textoSugerencias = tituloSugerencias.getElementsByTagName("p")[0]
+    textoSugerencias.innerText = stringUser + " (resultados)"
+};
+function ocultarSugerencias(){
+    document.getElementsByClassName("grid-sugerencias")[0].style.setProperty("display", "none")
+};
+function buscarSugerencias (){
+    for ( i = 0; i < 12; i++) {
+    fetch(URL_SUGERENCIAS + "api_key=" + API_KEY + "&q=happy")
+        .then((response)=>{
+            if(response.ok){
+                return response.json();
+            }
+        })
+        .then((bodyEnJson)=>{
+            resultadoSugerencias(bodyEnJson.data)    
+        });
+    }
+}
+function resultadoSugerencias (data){
+    let imagenSugerencia = document.createElement("img");
+    let divSugerencia = document.createElement("div");
+    let divTitulo = document.createElement("div");
+    let tituloGif = document.createElement("p");
+    let botonX = document.createElement("img");
+    let anchorVerMas = document.createElement("a");
+    let botonVerMas = document.createElement("div");
+    let txtVerMas = document.createElement("p");
+    let gridSugerencias = document.getElementsByClassName("grid-sugerencias")[0]
+    let tituloGifContenido = data.title;
+    let contenidoHashtag = tituloGifContenido.substring(0 , tituloGifContenido.indexOf(PALABRAGIF));
+    divSugerencia.setAttribute("class", "div-sugerencia");
+    divTitulo.setAttribute("class", "div-titulo-gif");
+    botonX.setAttribute("class", "boton-x")
+    botonX.setAttribute("src","/images/button3.svg")
+    imagenSugerencia.setAttribute("src", data.images['fixed_height'].url);
+    imagenSugerencia.setAttribute("class", "gif-sugerencia")
+    botonVerMas.setAttribute("class", "boton-ver-mas");
+    anchorVerMas.setAttribute("href", data.embed_url);
+    anchorVerMas.setAttribute("target", "_blank");
+    tituloGif.innerText = "#" + contenidoHashtag;
+    txtVerMas.innerText = "Ver más...";
+    divSugerencia.appendChild(imagenSugerencia);
+    botonVerMas.appendChild(txtVerMas)
+    anchorVerMas.appendChild(botonVerMas)
+    divSugerencia.appendChild(anchorVerMas);
+    divTitulo.appendChild(tituloGif)
+    divTitulo.appendChild(botonX)
+    divSugerencia.appendChild(divTitulo);
+    gridSugerencias.appendChild(divSugerencia)
+    botonX.addEventListener("click", ()=>{
+        divSugerencia.style.setProperty("display", "none")
+    })
+}
+buscarSugerencias();
+fetch(URL_TRENDING +  "api_key=" + API_KEY + "&limit=12")
+    .then((response)=>{
+        if(response.ok){
+            return response.json();
+        }
+    })
+    .then((bodyEnJson)=>{
+        funcionTendencias(bodyEnJson.data);
+    }
+);
+function funcionTendencias(data){
+    for ( i = 0; i < data.length; i++) {
+        let imagenTendencia = document.createElement("img");
+        let divTendencia = document.createElement("div");
+        let divHashtags = document.createElement("div");
+        let tituloGif = document.createElement("p");
+        let tituloGifContenido =  data[i].title;
+        let tituloGifFinal = tituloGifContenido.substring(0 , tituloGifContenido.indexOf(PALABRAGIF));
+        let urlGif = document.createElement("a");
+        urlGif.setAttribute("href", data[i].embed_url);
+        urlGif.setAttribute("target","_blank");
+        divTendencia.setAttribute("class", "div-tendencia");
+        divHashtags.setAttribute("class", "div-hashtags");
+        tituloGif.innerText = "#" + tituloGifFinal;
+        imagenTendencia.setAttribute("src", data[i].images['fixed_height'].url);
+        imagenTendencia.style.setProperty("height", "298px");
+        if(imagenTendencia.width / imagenTendencia.height >= 1.6){
+            urlGif.style.setProperty("grid-column", "auto / span 2")
+            divTendencia.style.setProperty("width", "596px");
+
+        }else{
+            divTendencia.style.setProperty("width", "298px");
+        }
+        divHashtags.appendChild(tituloGif)
+        divTendencia.appendChild(imagenTendencia);
+        urlGif.appendChild(divTendencia)
+        divTendencia.appendChild(divHashtags);
+        document.getElementById("grid-tendencias").appendChild(urlGif)
+    }
+};
+function guardarThemeEnLocalStorage (){
+    if (document.body.classList.contains("modoOscuro")){
+        localStorage.setItem("modoOscuro", "true");
+    }else {
+        localStorage.setItem("modoOscuro", "false"); 
+    }
+};
+function tomarThemeDeLocalStorage(){
+    if(localStorage.getItem("modoOscuro") === "true"){
+        document.body.classList.add("modoOscuro");
+        logo.setAttribute("src", "/images/gifOF_logo_dark.png");
+    }else{
+        document.body.classList.remove("modoOscuro"); 
+    }
+}
+tomarThemeDeLocalStorage();
+//EVENT LISTENERS
+botonModoOscuro.addEventListener("click", ()=>{
+    document.body.setAttribute("class","modoOscuro");
+    menuEstilos.setAttribute("class", "menu-estilos");
+    logo.setAttribute("src", "/images/gifOF_logo_dark.png")
+})
+botonModoOscuro.addEventListener("click", guardarThemeEnLocalStorage);
+botonModoClaro.addEventListener("click", ()=>{
+    document.body.setAttribute("class","modoClaro");
+    menuEstilos.setAttribute("class", "menu-estilos");
+    logo.setAttribute("src", "/images/gifOF_logo.png");
+    document.body.classList.remove("modoOscuro");
+});
+botonModoClaro.addEventListener("click", guardarThemeEnLocalStorage);
+BOTON_ESTILOS.addEventListener("click", FUNCION_MENU_SHOW);
+botonMisGuifos.addEventListener("click", mostrarMisGuifos);
+botonPosiblesResultados1.addEventListener("click", ()=>{
+    stringUser = botonPosiblesResultados1.innerText;
+})
+botonPosiblesResultados2.addEventListener("click", ()=>{
+    stringUser = botonPosiblesResultados2.innerText;
+})
+botonPosiblesResultados3.addEventListener("click", ()=>{
+    stringUser = botonPosiblesResultados3.innerText;
+})
+SEARCH.addEventListener("keyup", autocompletar);
+SEARCH.addEventListener("keyup", mostrarSugerencias);
+BOTON_SEARCH.addEventListener("click", ocultarAutocomplete);
+BOTON_SEARCH.addEventListener("click", ocultarTendencias);
+BOTON_SEARCH.addEventListener("click", pushBusqueda);
+BOTON_SEARCH.addEventListener("click", buscarGif);  
+BOTON_SEARCH.addEventListener("click", cambiarTitulo);
+BOTON_SEARCH.addEventListener("click", ocultarSugerencias);
+////////////BUSCAR CON POSIBLES RESULTADOS BOTON 1///////////
+botonPosiblesResultados1.addEventListener("click", ocultarAutocomplete);
+botonPosiblesResultados1.addEventListener("click", ocultarTendencias);
+botonPosiblesResultados1.addEventListener("click", pushBusqueda);
+botonPosiblesResultados1.addEventListener("click", buscarGif);  
+botonPosiblesResultados1.addEventListener("click", cambiarTitulo);
+botonPosiblesResultados1.addEventListener("click", ocultarSugerencias);
+////////////BUSCAR CON POSIBLES RESULTADOS BOTON 2///////////
+botonPosiblesResultados2.addEventListener("click", ocultarAutocomplete);
+botonPosiblesResultados2.addEventListener("click", ocultarTendencias);
+botonPosiblesResultados2.addEventListener("click", pushBusqueda);
+botonPosiblesResultados2.addEventListener("click", buscarGif);  
+botonPosiblesResultados2.addEventListener("click", cambiarTitulo);
+botonPosiblesResultados2.addEventListener("click", ocultarSugerencias);
+////////////BUSCAR CON POSIBLES RESULTADOS BOTON 3///////////
+botonPosiblesResultados3.addEventListener("click", ocultarAutocomplete);
+botonPosiblesResultados3.addEventListener("click", ocultarTendencias);
+botonPosiblesResultados3.addEventListener("click", pushBusqueda);
+botonPosiblesResultados3.addEventListener("click", buscarGif);  
+botonPosiblesResultados3.addEventListener("click", cambiarTitulo);
+botonPosiblesResultados3.addEventListener("click", ocultarSugerencias);
+botonMisGuifos.addEventListener("click", ()=>{
+    let cuadroDeBusqueda = document.getElementsByClassName("cuadro-buscar")[0];
+    cuadroDeBusqueda.style.setProperty("display", "none");
+    let gridSugerencias = document.getElementsByClassName("grid-sugerencias")[0]
+    gridSugerencias.style.setProperty("display", "none");
+    let tituloSugerencias = document.getElementsByClassName("sugerencias")[0];
+    tituloSugerencias.style.setProperty("display", "none");
+    let gridTendencias = document.getElementById("grid-tendencias");
+    gridTendencias.style.setProperty("display", "none");
+    let tituloTendencias = document.getElementsByClassName("texto-tendencias")[0];
+    tituloTendencias.style.setProperty("display", "none");
+    let tituloMisGuifos = document.getElementsByClassName("titulo-mis-guifos")[0];
+    tituloMisGuifos.style.setProperty("display", "block");
+    gridMisGuifos.style.setProperty("display", "block");
+})
---
+++ scriptGrabarGifOs.js
@@ -0,0 +1,201 @@
+///URL ENDPOINTS
+const URL_GIFS_BY_USER = "https://api.github.com/users/sosacaro";
+const URL_UPLOAD = "https://upload.giphy.com/v1/gifs"
+const GIPHY_USER_NAME = "SosaCaro";
+const GIPHY_API_KEY = "E83LFC5hFXEDnC6KbSjVge3k89s0q5dl";
+const URL_SEARCH_BY_ID = "https://api.giphy.com/v1/gifs/";
+const URL_FINAL_POST = URL_UPLOAD + "?api_key=" + GIPHY_API_KEY + "&username=" + GIPHY_USER_NAME;
+//OBJETOS DEL DOM
+let logo = document.getElementById("logo");
+let createBox = document.getElementById("create-box");
+let tituloMisGUifos = document.getElementById("title-mis-guifos");
+let gridMisGuifos = document.getElementsByClassName("grid-mis-guifos")[0];
+let cartel = document.getElementById("cartel-oops");
+let tituloCreateBox = document.getElementsByClassName("titulo-cuadro-video")[0];
+let txtTitulo = tituloCreateBox.getElementsByTagName("p")[0];
+let videoBox = document.getElementById("video-box");
+let divSubiendo = document.getElementById("subiendo");
+let cartelExito = document.getElementById("cartel-exito");
+let divGifExitoso = document.getElementById("gif-exitoso");
+const LIVE_VIDEO = document.getElementById("live-video");
+const VIEWPORT = document.getElementById("viewport");
+//BOTONES
+let botonComenzar = document.getElementsByClassName("boton-comenzar")[0];
+let btnCapture = document.getElementById("btn-capture")
+let btnCaptureClicked = document.getElementById("btn-capture-clicked");
+let btnRepetir = document.getElementsByClassName("btn-repetir")[0];
+let btnSubir = document.getElementsByClassName("btn-subir") [0];
+let btnCancelar = document.getElementsByClassName("btn-cancelar")[0];
+let botonListo = document.getElementsByClassName("boton-listo")[0];
+//VARIABLES A LLENAR
+let misGifs =[];
+let gifGrabado = "";
+let blob;
+let stream;
+let recorder; 
+//FUNCIONES
+function tomarThemeDeLocalStorage(){
+    if(localStorage.getItem("modoOscuro") === "true"){
+        document.body.classList.add("modoOscuro");
+        logo.setAttribute("src", "/images/gifOF_logo_dark.png");
+    }else{
+        document.body.classList.remove("modoOscuro"); 
+    }
+}
+tomarThemeDeLocalStorage();
+function mostrarVideoLive(){
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+    })
+    .then((response)=>{
+    stream = response,    
+    LIVE_VIDEO.srcObject = stream
+    })
+}
+async function GrabarGif (){
+    recorder = new RecordRTCPromisesHandler(stream, {
+        type: "gif",
+        frameRate: 1,
+        quality: 10,
+        width: 838,
+        height: 440,
+    });
+    recorder.startRecording();
+    const sleep = m => new Promise(r => setTimeout(r, m));
+    await sleep(7000);
+    await recorder.stopRecording();
+    blob = await recorder.getBlob();
+    mostrarVistaPrevia(blob)
+}
+function mostrarVistaPrevia(blob){
+    gifGrabado = document.createElement("img");
+    let gif = URL.createObjectURL(blob)
+    gifGrabado.src = gif;
+    LIVE_VIDEO.style.setProperty("display", "none");
+    VIEWPORT.appendChild(gifGrabado)
+    txtTitulo.innerText = "Vista Previa";
+    btnCaptureClicked.style.setProperty("display", "none")
+    let botonRepetir = document.getElementsByClassName("btn-repetir")[0];
+    let botonSubir = document.getElementsByClassName("btn-subir") [0];
+    botonRepetir.style.setProperty("display", "block");
+    botonSubir.style.setProperty("display", "block")
+}
+function repetirCaptura(){
+    txtTitulo.innerText = "Un Chequeo Antes de Empezar";
+    btnRepetir.style.setProperty("display", "none");
+    btnSubir.style.setProperty("display", "none");
+    btnCapture.style.setProperty("display", "block");
+    gifGrabado.style.setProperty("display", "none")
+    LIVE_VIDEO.style.setProperty("display", "block");
+    mostrarVideoLive;
+}
+async function subirGif(){
+    let form = new FormData();
+    form.append("file", blob, "miGif.gif");
+    const response = await fetch(URL_FINAL_POST,{
+        mode: "cors",
+        method: "POST",
+        body: form,
+    })
+    const responseParseado = await response.json();
+    let gifId = responseParseado.data.id;
+    if(localStorage.length == 1){
+        misGifs.push(gifId);
+        localStorage.setItem("misGifs", JSON.stringify(misGifs));
+    }else{
+        misGifs = JSON.parse(localStorage.getItem("misGifs"));
+        misGifs.push(gifId);
+        localStorage.setItem("misGifs", JSON.stringify(misGifs));
+    }
+}
+function mostrarCuadroSubiendo(){
+    VIEWPORT.style.setProperty("display", "none");
+    txtTitulo.innerText = "Subiendo Guifo";
+    let btnCancelar = document.getElementsByClassName("btn-cancelar")[0];
+    btnCancelar.style.setProperty("display", "block");
+    divSubiendo.style.setProperty("display", "block");
+    btnRepetir.style.setProperty("display", "none");
+    btnSubir.style.setProperty("display", "none");
+    videoBox.appendChild(divSubiendo);
+    videoBox.appendChild(btnCancelar);
+}
+function mostrarMisGuifos(){
+    if(localStorage.getItem("misGifs") == null){
+        cartel.style.setProperty("display", "block");
+    }else{
+        misGifs = JSON.parse(localStorage.getItem("misGifs"));
+        misGifs.forEach(hacerFetchMisGuifos);
+    }
+}
+function hacerFetchMisGuifos (element){
+    fetch(URL_SEARCH_BY_ID + element +"?api_key=" + GIPHY_API_KEY )
+    .then((response)=>{
+        if(response.ok){
+            return response.json();
+        }
+    })
+    .then((bodyEnJson)=>{
+        displayMisGuifos(bodyEnJson.data);
+    })
+}
+function displayMisGuifos(data){
+        let gifResultado = document.createElement("img");
+        let urlGif = document.createElement("a");
+        urlGif.setAttribute("href", data.embed_url)
+        urlGif.setAttribute("target", "_blank");
+        gifResultado.setAttribute("src", data.images["fixed_height"].url)
+        urlGif.appendChild(gifResultado)
+        gridMisGuifos.appendChild(urlGif);
+}
+mostrarMisGuifos();
+function eliminarUltimoPush(){
+    misGifs = JSON.parse(localStorage.getItem("misGifs"));
+    misGifs.pop();
+    localStorage.setItem("misGifs", JSON.stringify(misGifs));
+}
+function ocultarCartelSubiendo(){
+    videoBox.style.setProperty("display", "none");
+    createBox.style.setProperty("display", "block");
+    gridMisGuifos.style.setProperty("display", "block");
+    tituloMisGUifos.style.setProperty("display", "block");
+}
+function mostrarCartelExito(){
+    cartelExito.style.setProperty("display", "block");
+    videoBox.style.setProperty("display", "none");
+    tituloMisGUifos.style.setProperty("display", "block");
+    gridMisGuifos.style.setProperty("display", "block");
+    gifGrabado.style.setProperty("width", "365px");
+    gifGrabado.style.setProperty("heigth", "191px");
+    divGifExitoso.appendChild(gifGrabado);
+}
+//EVENT LISTENERS
+botonComenzar.addEventListener("click", ()=>{
+    createBox.style.setProperty("display", "none");
+    videoBox.style.setProperty("display", "block");
+    tituloMisGUifos.style.setProperty("display", "none");
+    gridMisGuifos.style.setProperty("display", "none");
+})
+botonComenzar.addEventListener("click", mostrarVideoLive);
+btnCapture.addEventListener("click", ()=>{
+    btnCaptureClicked.style.setProperty("display", "block")
+    btnCapture.style.setProperty("display", "none")
+  });
+btnCapture.addEventListener("click", GrabarGif)
+btnCapture.addEventListener("click", ()=>{
+    txtTitulo.innerText = "Capturando Tu Guifo"
+});
+btnRepetir.addEventListener("click", repetirCaptura);
+btnSubir.addEventListener("click", mostrarCuadroSubiendo);
+btnSubir.addEventListener("click", subirGif);
+btnSubir.addEventListener("click", ()=> setTimeout(mostrarCartelExito, 4000));
+btnCancelar.addEventListener("click", eliminarUltimoPush);
+btnCancelar.addEventListener("click", repetirCaptura);
+btnCancelar.addEventListener("click", ocultarCartelSubiendo);
+botonListo.addEventListener("click", ()=>{
+    cartelExito.style.setProperty("display", "none")
+});
+botonListo.addEventListener("click", ()=>{
+    gridMisGuifos.innerHTML = "";
+})
+botonListo.addEventListener("click", mostrarMisGuifos);
